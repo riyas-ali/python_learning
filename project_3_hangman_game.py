9c3f5cd4124c7bd38cd38ac3455f9af18365a369
Riyas Ali MK <riyasalimkokl@gmail.com>
2023-05-08 23:21:39 +0530

    Hangman game project

---
+++ project_3_hangman_game.py
@@ -0,0 +1,81 @@
+import random
+
+word_list = ["apple", "banana", "potato", "Python", "Swift", "C++"]
+chosen_word = random.choice(word_list)
+display = []
+lives = 6
+game_over = False
+HANGMANPICS = ['''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========''']
+HANGMANPICS.reverse()
+for letter in chosen_word:
+    display += "_"
+print(display)
+
+while not game_over:
+    guessed_letter = input("Guess a letter: ").lower()
+    for position in range(len(chosen_word)):
+        letter = chosen_word[position]
+        if letter == guessed_letter:
+            display[position] = guessed_letter
+    print(display)
+    if guessed_letter not in chosen_word:
+        lives -= 1
+        if lives == 0:
+            game_over = True
+            print()
+            print("You lose..!")
+    if "_" not in display:
+        game_over = True
+        print()
+        print("You Win...!")
+    print(HANGMANPICS[lives])
+
