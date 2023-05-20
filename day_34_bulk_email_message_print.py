b520d5fbefdac30929fbc8c29a918b5c709045a7
Riyas Ali MK <riyasalimkokl@gmail.com>
2023-05-20 19:54:02 +0530

    print email list with index

---
+++ day_34_bulk_email_message_print.py
@@ -0,0 +1,47 @@
+import os, time
+
+list_of_email = []
+
+
+def prettyPrint():
+    os.system("clear")
+    print("listOfEmail")
+    print()
+    counter = 1
+    for email in list_of_email:
+        print(f"{counter}: {email}")
+        counter += 1
+    time.sleep(1)
+
+
+def spam(max):
+    for i in range(0, max):
+        print(f"""Email {i + 1}
+
+Dear {list_of_email[i]} It has come to our attention that you're missing out on the amazing Replit 100 days of code. We 
+insist you do it right away. If you don't we will pass on your email address to every spammer we've ever encountered 
+and also sign you up to the My Little Pony newsletter, because that's neat. We might just do that anyway.
+
+Love and hugs,
+
+Ian Spammington III""")
+        time.sleep(1)
+        os.system("clear")
+
+
+while True:
+    print("SPAMMER Inc.")
+    menu = input("1: Add email\n2: Remove email\n3: Show emails\n4: Get SPAMMING\n> ")
+    if menu == "1":
+        email = input("Email > ")
+        list_of_email.append(email)
+    elif menu == "2":
+        email = input("Email > ")
+        if email in list_of_email:
+            list_of_email.remove(email)
+    elif menu == "3":
+        prettyPrint()
+    elif menu == "4":
+        spam(len(list_of_email))
+    time.sleep(1)
+    os.system("clear")
